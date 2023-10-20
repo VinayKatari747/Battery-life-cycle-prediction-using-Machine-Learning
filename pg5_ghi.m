04433eda7d4ba979d054a73da98cb322ad47e2ba
VinayKatari747 <144598965+VinayKatari747@users.noreply.github.com>
2023-10-20 16:42:03 +0530

    Delete pg5_ghi.m

--- pg5_ghi.m
+++
@@ -1,9 +0,0 @@
-% dQ/dV vs Volatge graphs g,h,i
-dQ1 = batch(30).cycles(10).discharge_dQdV;
-dQ2 = batch(30).cycles(100).discharge_dQdV;
-V = batch(30).cycles(500).V(1:1000);
-hold on
-plot(V,dQ1);
-plot(V,dQ2);
-hold off
-legend
