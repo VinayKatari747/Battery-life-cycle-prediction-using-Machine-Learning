f7bc455001ceda0516cf1309e69e29565005566f
Vinay Katari <144598965+VinayKatari747@users.noreply.github.com>
2024-05-18 21:14:36 +0530

    Delete Matlab codes/mean_variance_calculation.m

--- Matlab codes/mean_variance_calculation.m
+++
@@ -1,5 +0,0 @@
-Qd = batch(1).cycles(2).Qd; % Replace [your_Qd_data] with your actual data
-
-Qd_without_zeros = Qd(Qd ~= 0);
-mean_without_zeros = mean(Qd_without_zeros); % Calculate mean excluding zero values
-min_value_without_zeros = min(Qd_without_zeros); % Calculate minimum value excluding zero values
